8dbb0af7dc8488fb232594c78a7aa748567836ee
Zayed Fahim <zayedfahim33.cse@gmail.com>
2023-01-18 01:13:36 +0600

    make responsive in progress

--- src/pages/Banner/Banner.js
+++ src/pages/Banner/Banner.js
@@ -10,74 +10,80 @@ import cv from "../../assets/cv/Sayed Asif Zayed.png";
 
 const Banner = () => {
     return (
-      <div className="h-[790px] bg-gradient-to-r from-[#2C2C2C] to-[#1C1C1C]">
-        <div className="grid grid-cols-2 container mx-auto">
-          <div className="flex flex-col relative top-60">
-            <h1 className="text-[#E4E4E4] text-5xl">HELLO</h1>
-            <div className="leading-[72px] mt-6 w-[660px] h-[136px]">
-              <h1 className="text-[#E4E4E4] text-7xl font-bold">
-                I'AM{" "}
-                <span className="text-[#00C4F0] font-bold">
-                  <Typewriter
-                    words={[
-                      "SAYED ASIF ZAYED",
-                      "FRONT-END DEVELOPER",
-                      "MERN STACK DEVELOPER",
-                    ]}
-                    loop
-                    cursor
-                    cursorStyle="_"
-                    typeSpeed={70}
-                    deleteSpeed={80}
-                    delaySpeed={1000}
-                  />
-                </span>
-              </h1>
+      <div>
+        <div className="h-auto lg:h-[790px] bg-gradient-to-r from-[#2C2C2C] to-[#1C1C1C]">
+          <div className="flex flex-col-reverse gap-0 lg:grid lg:grid-cols-2 container mx-auto">
+            <div className="flex flex-col relative  lg:top-60">
+              <h1 className="text-[#E4E4E4] text-4xl m-5 lg:m-0 lg:text-5xl">HELLO</h1>
+              <div className="leading-[72px] lg:mt-6 lg:w-[660px] lg:h-[136px] h-[100px]">
+                <h1 className="text-[#E4E4E4] text-4xl lg:text-7xl mb-4 ml-5 lg:ml-0 lg:mb-0 font-bold">
+                  I'AM{" "}
+                  <span className="text-[#00C4F0] font-bold">
+                    <Typewriter
+                      words={[
+                        "SAYED ASIF ZAYED",
+                        "FRONT-END DEVELOPER",
+                        "MERN STACK DEVELOPER",
+                      ]}
+                      loop
+                      cursor
+                      cursorStyle="_"
+                      typeSpeed={70}
+                      deleteSpeed={80}
+                      delaySpeed={1000}
+                    />
+                  </span>
+                </h1>
+              </div>
+              <p className="text-[#E4E4E4] lg:font-[18px] ml-5 mb-4 lg:ml-0 lg:mb-0 lg:mt-8">
+                I've been doing web design, front-end and back-end development{" "}
+                <br /> for a year now. Do you need a website design, site
+                layout, or maybe
+                <br /> a turnkey website? Then contact me to hire for your work.
+              </p>
+              <div className="flex gap-10 ml-5 mb-4 lg:ml-0 lg:mb-0 lg:mt-8">
+                <Link>
+                  <PhotoProvider>
+                    <PhotoView src={cv}>
+                      <button className="text-black h-[40px] px-8 lg:px-10 font-bold bg-gradient-to-r from-[#E2A300] to-[#E29500] rounded-[6px] shadow-[0_2px_13px_rgba(226,158,0,0.48)]">
+                        Resume
+                      </button>
+                    </PhotoView>
+                  </PhotoProvider>
+                </Link>
+                <Link>
+                  <button className="text-black h-[40px] px-8 lg:px-10 font-bold bg-gradient-to-r from-[#E2A300] to-[#E29500] rounded-[6px] shadow-[0_2px_13px_rgba(226,158,0,0.48)]">
+                    Hire Me
+                  </button>
+                </Link>
+              </div>
             </div>
-            <p className="text-[#E4E4E4] font-[18px] mt-8">
-              I've been doing web design, front-end and back-end development{" "}
-              <br /> for a year now. Do you need a website design, site layout,
-              or maybe
-              <br /> a turnkey website? Then contact me to hire for your work.
-            </p>
-            <div className="flex gap-10 mt-8">
-              <Link>
-                <PhotoProvider>
-                  <PhotoView src={cv}>
-                    <button className="text-black h-[40px] px-10 font-bold bg-gradient-to-r from-[#E2A300] to-[#E29500] rounded-[6px] shadow-[0_2px_13px_rgba(226,158,0,0.48)]">
-                      Resume
-                    </button>
-                  </PhotoView>
-                </PhotoProvider>
-              </Link>
-              <Link>
-                <button className="text-black h-[40px] px-10 font-bold bg-gradient-to-r from-[#E2A300] to-[#E29500] rounded-[6px] shadow-[0_2px_13px_rgba(226,158,0,0.48)]">
-                  Hire Me
-                </button>
-              </Link>
+            <div>
+              <div className="w-[390px] h-[390px] lg:w-[575px] lg:h-[575px] border-solid border-[50px] right-0 lg:right-auto top-[120px] lg:top-auto lg:border-[67px] border-[#00C4F0] drop-shadow-[0_0_152px_rgba(0,196,240,0.42)] rounded-[50%] absolute lg:bottom-[121px]"></div>
+              <div className="absolute bottom-[121px] right-[25px] lg:right-auto top-[30px] lg:top-auto">
+                <img
+                  className="h-[480px] lg:h-[685px] lg:ml-5"
+                  src={me}
+                  alt="me-1"
+                />
+              </div>
             </div>
-          </div>
-          <div>
-            <div className="w-[575px] h-[575px] border-solid border-[67px] border-[#00C4F0] drop-shadow-[0_0_152px_rgba(0,196,240,0.42)] rounded-[50%]  absolute bottom-[121px]"></div>
-            <div className="absolute bottom-[121px]">
-              <img className="h-[685px] ml-5" src={me} alt="me-1" />
-            </div>
-          </div>
-          <div>
-            <div className="w-[90px] h-[90px] bg-gradient-to-r from-[#414141] to-[#2D2D2D] text-3xl font-bold rounded-[50%] flex justify-center items-center relative left-[760px] top-[220px]">
-              <FaHtml5 className="h-16 w-16 text-[#00C4F0]"></FaHtml5>
-            </div>
-            <div className="w-[90px] h-[90px] bg-gradient-to-r from-[#414141] to-[#2D2D2D] text-3xl font-bold rounded-[50%] flex justify-center items-center relative left-[730px] top-[-85px]">
-              <FaReact className="h-16 w-16 text-[#00C4F0]"></FaReact>
-            </div>
-            <div className="w-[90px] h-[90px] bg-gradient-to-r from-[#414141] to-[#2D2D2D] text-3xl font-bold rounded-[50%] flex justify-center items-center relative left-[850px] top-[-345px]">
-              <SiFirebase className="h-16 w-16 text-[#00C4F0]"></SiFirebase>
-            </div>
-            <div className="w-[90px] h-[90px] bg-gradient-to-r from-[#414141] to-[#2D2D2D] text-3xl font-bold rounded-[50%] flex justify-center items-center relative left-[1190px] top-[-425px]">
-              <FaNode className="h-16 w-16 text-[#00C4F0]"></FaNode>
-            </div>
-            <div className="w-[90px] h-[90px] bg-gradient-to-r from-[#414141] to-[#2D2D2D] text-3xl font-bold rounded-[50%] flex justify-center items-center relative left-[1295px] top-[-330px]">
-              <FaCode className="h-16 w-16 text-[#00C4F0]"></FaCode>
+            <div className="invisible md:visible lg:visible">
+              <div className="w-[90px] h-[90px] bg-gradient-to-r from-[#414141] to-[#2D2D2D] text-3xl font-bold rounded-[50%] flex justify-center items-center relative left-[760px] top-[220px]">
+                <FaHtml5 className="h-16 w-16 text-[#00C4F0]"></FaHtml5>
+              </div>
+              <div className="w-[90px] h-[90px] bg-gradient-to-r from-[#414141] to-[#2D2D2D] text-3xl font-bold rounded-[50%] flex justify-center items-center relative left-[730px] top-[-85px]">
+                <FaReact className="h-16 w-16 text-[#00C4F0]"></FaReact>
+              </div>
+              <div className="w-[90px] h-[90px] bg-gradient-to-r from-[#414141] to-[#2D2D2D] text-3xl font-bold rounded-[50%] flex justify-center items-center relative left-[850px] top-[-345px]">
+                <SiFirebase className="h-16 w-16 text-[#00C4F0]"></SiFirebase>
+              </div>
+              <div className="w-[90px] h-[90px] bg-gradient-to-r from-[#414141] to-[#2D2D2D] text-3xl font-bold rounded-[50%] flex justify-center items-center relative left-[1190px] top-[-425px]">
+                <FaNode className="h-16 w-16 text-[#00C4F0]"></FaNode>
+              </div>
+              <div className="w-[90px] h-[90px] bg-gradient-to-r from-[#414141] to-[#2D2D2D] text-3xl font-bold rounded-[50%] flex justify-center items-center relative left-[1295px] top-[-330px]">
+                <FaCode className="h-16 w-16 text-[#00C4F0]"></FaCode>
+              </div>
             </div>
           </div>
         </div>
--- src/pages/Experience/Experience.js
+++ src/pages/Experience/Experience.js
@@ -9,7 +9,7 @@ const Experience = () => {
       className="bg-gradient-to-r from-[#414141] to-[#2D2D2D]"
       id="experience"
     >
-      <h1 className="text-[#E4E4E4] text-5xl font-bold text-center pt-28">
+      <h1 className="text-[#E4E4E4] text-[38px] lg:text-5xl font-bold text-center pt-10 lg:pt-28">
         <span className="text-[#00C4F0] font-bold">
           <Typewriter
             words={["EXPERIENCED AT"]}
@@ -23,48 +23,48 @@ const Experience = () => {
         </span>
       </h1>
       <div className="container mx-auto">
-        <div className="flex justify-center pt-16 gap-10">
-          <div className="bg-[#2A2A2A] rounded-[29px] shadow-[0_4px_6px_rgba(0,0,0,0.47)] w-[230px] h-[230px] flex flex-col justify-center items-center gap-[34px]">
+        <div className="flex-wrap flex lg:flex-nowrap justify-center pt-10 lg:pt-16 gap-10">
+          <div className="bg-[#2A2A2A] rounded-[29px] shadow-[0_4px_6px_rgba(0,0,0,0.47)] w-[150px] h-[150px] lg:w-[230px] lg:h-[230px] flex flex-col justify-center items-center lg:gap-[34px]">
             <FaHtml5 className="h-[75px] w-[75px] text-[#00C4F0]"></FaHtml5>
-            <h1 className="text-white text-2xl">HTML5</h1>
+            <h1 className="text-white lg:text-2xl">HTML5</h1>
           </div>
-          <div className="bg-[#2A2A2A] rounded-[29px] shadow-[0_4px_6px_rgba(0,0,0,0.47)] w-[230px] h-[230px] flex flex-col justify-center items-center gap-[34px]">
+          <div className="bg-[#2A2A2A] rounded-[29px] shadow-[0_4px_6px_rgba(0,0,0,0.47)] w-[150px] h-[150px] lg:w-[230px] lg:h-[230px] flex flex-col justify-center items-center lg:gap-[34px]">
             <FaCss3Alt className="h-[75px] w-[75px] text-[#00C4F0]"></FaCss3Alt>
-            <h1 className="text-white text-2xl">CSS3</h1>
+            <h1 className="text-white lg:text-2xl">CSS3</h1>
           </div>
-          <div className="bg-[#2A2A2A] rounded-[29px] shadow-[0_4px_6px_rgba(0,0,0,0.47)] w-[230px] h-[230px] flex flex-col justify-center items-center gap-[34px]">
+          <div className="bg-[#2A2A2A] rounded-[29px] shadow-[0_4px_6px_rgba(0,0,0,0.47)] w-[150px] h-[150px] lg:w-[230px] lg:h-[230px] flex flex-col justify-center items-center lg:gap-[34px]">
             <SiJavascript className="h-[75px] w-[75px] text-[#00C4F0]"></SiJavascript>
-            <h1 className="text-white text-2xl">Java Script</h1>
+            <h1 className="text-white lg:text-2xl">Java Script</h1>
           </div>
-          <div className="bg-[#2A2A2A] rounded-[29px] shadow-[0_4px_6px_rgba(0,0,0,0.47)] w-[230px] h-[230px] flex flex-col justify-center items-center gap-[34px]">
+          <div className="bg-[#2A2A2A] rounded-[29px] shadow-[0_4px_6px_rgba(0,0,0,0.47)] w-[150px] h-[150px] lg:w-[230px] lg:h-[230px] flex flex-col justify-center items-center lg:gap-[34px]">
             <FaReact className="h-[75px] w-[75px] text-[#00C4F0]"></FaReact>
-            <h1 className="text-white text-2xl">React</h1>
+            <h1 className="text-white lg:text-2xl">React</h1>
           </div>
         </div>
 
-        <div className="flex justify-center gap-10 mt-10">
-          <div className="bg-[#2A2A2A] rounded-[29px] shadow-[0_4px_6px_rgba(0,0,0,0.47)] w-[230px] h-[230px] flex flex-col justify-center items-center gap-[34px]">
+        <div className="flex flex-wrap lg:flex-nowrap justify-center gap-10 mt-10">
+          <div className="bg-[#2A2A2A] rounded-[29px] shadow-[0_4px_6px_rgba(0,0,0,0.47)] w-[150px] h-[150px] lg:w-[230px] lg:h-[230px] flex flex-col justify-center items-center lg:gap-[34px]">
             <SiMongodb className="h-[75px] w-[75px] text-[#00C4F0]"></SiMongodb>
-            <h1 className="text-white text-2xl">mongo DB</h1>
+            <h1 className="text-white lg:text-2xl">mongo DB</h1>
           </div>
-          <div className="bg-[#2A2A2A] rounded-[29px] shadow-[0_4px_6px_rgba(0,0,0,0.47)] w-[230px] h-[230px] flex flex-col justify-center items-center gap-[34px]">
+          <div className="bg-[#2A2A2A] rounded-[29px] shadow-[0_4px_6px_rgba(0,0,0,0.47)] w-[150px] h-[150px] lg:w-[230px] lg:h-[230px] flex flex-col justify-center items-center lg:gap-[34px]">
             <SiExpress className="h-[75px] w-[75px] text-[#00C4F0]"></SiExpress>
-            <h1 className="text-white text-2xl">Express JS</h1>
+            <h1 className="text-white lg:text-2xl">Express JS</h1>
           </div>
-          <div className="bg-[#2A2A2A] rounded-[29px] shadow-[0_4px_6px_rgba(0,0,0,0.47)] w-[230px] h-[230px] flex flex-col justify-center items-center gap-[34px]">
+          <div className="bg-[#2A2A2A] rounded-[29px] shadow-[0_4px_6px_rgba(0,0,0,0.47)] w-[150px] h-[150px] lg:w-[230px] lg:h-[230px] flex flex-col justify-center items-center lg:gap-[34px]">
             <FaNode className="h-[75px] w-[75px] text-[#00C4F0]"></FaNode>
-            <h1 className="text-white text-2xl">Node JS</h1>
+            <h1 className="text-white lg:text-2xl">Node JS</h1>
           </div>
         </div>
 
-        <div className="flex justify-center gap-10 pb-32 mt-10">
-          <div className="bg-[#2A2A2A] rounded-[29px] shadow-[0_4px_6px_rgba(0,0,0,0.47)] w-[230px] h-[230px] flex flex-col justify-center items-center gap-[34px]">
+        <div className="flex flex-wrap lg:flex-nowrap justify-center gap-10 pb-10 lg:pb-32 mt-10">
+          <div className="bg-[#2A2A2A] rounded-[29px] shadow-[0_4px_6px_rgba(0,0,0,0.47)] w-[150px] h-[150px] lg:w-[230px] lg:h-[230px] flex flex-col justify-center items-center lg:gap-[34px]">
             <SiFirebase className="h-[75px] w-[75px] text-[#00C4F0]"></SiFirebase>
-            <h1 className="text-white text-2xl">Firebase</h1>
+            <h1 className="text-white lg:text-2xl">Firebase</h1>
           </div>
-          <div className="bg-[#2A2A2A] rounded-[29px] shadow-[0_4px_6px_rgba(0,0,0,0.47)] w-[230px] h-[230px] flex flex-col justify-center items-center gap-[34px]">
+          <div className="bg-[#2A2A2A] rounded-[29px] shadow-[0_4px_6px_rgba(0,0,0,0.47)] w-[150px] h-[150px] lg:w-[230px] lg:h-[230px] flex flex-col justify-center items-center lg:gap-[34px]">
             <SiVercel className="h-[75px] w-[75px] text-[#00C4F0]"></SiVercel>
-            <h1 className="text-white text-2xl">Vercel</h1>
+            <h1 className="text-white lg:text-2xl">Vercel</h1>
           </div>
         </div>
       </div>
