8b1f51d52846de32006fd1306e2e195310435286
Zayed Fahim <zayedfahim33.cse@gmail.com>
2023-01-17 13:44:26 +0600

    chnage something

--- src/pages/Navbar/Navbar.js
+++ src/pages/Navbar/Navbar.js
@@ -7,7 +7,11 @@ const Navbar = () => {
       <div className="bg-gradient-to-r from-[#2C2C2C] to-[#1C1C1C] sticky top-0 z-[2]">
         <div className="container mx-auto navbar h-5">
           <div className="flex-1">
-            <NavHashLink smooth to="/home#" className="text-white text-4xl font-bold">
+            <NavHashLink
+              smooth
+              to="/home#"
+              className="text-white text-4xl font-bold"
+            >
               <span className="text-[#00C4F0] font-bold">zayed</span>DEV
               <span className="font-bold text-7xl text-[#E2A300] absolute top-[-20px]">
                 .
@@ -17,26 +21,43 @@ const Navbar = () => {
           <div className="text-white">
             <ul className="flex gap-12 text-[18px] font-bold">
               <li className="hover:text-[#00C4F0]">
-                <NavHashLink smooth to="home/#root">Home</NavHashLink>
+                <NavHashLink smooth to="home/#root">
+                  Home
+                </NavHashLink>
               </li>
               <li className="hover:text-[#00C4F0]">
-                <NavHashLink smooth to="home/#experience">Experience</NavHashLink>
+                <NavHashLink smooth to="home/#experience">
+                  Experience
+                </NavHashLink>
               </li>
               <li className="hover:text-[#00C4F0]">
-                <NavHashLink smooth to="home/#about-me">About me</NavHashLink>
+                <NavHashLink smooth to="home/#about-me">
+                  About me
+                </NavHashLink>
               </li>
               <li className="hover:text-[#00C4F0]">
-                <NavHashLink smooth to="home/#projects">Projects</NavHashLink>
+                <NavHashLink smooth to="home/#projects">
+                  Projects
+                </NavHashLink>
               </li>
               <li className="hover:text-[#00C4F0]">
-                <NavHashLink smooth to="/blog">Blogs</NavHashLink>
+                <NavHashLink smooth to="/blog">
+                  Blogs
+                </NavHashLink>
               </li>
               <li className="hover:text-[#00C4F0]">
-                <NavHashLink smooth to="home/#contact">Contact</NavHashLink>
+                <NavHashLink smooth to="home/#contact">
+                  Contact
+                </NavHashLink>
               </li>
             </ul>
           </div>
-          <Link to="../../assets/cv/Sayed Asif Zayed.pdf" target="_blank" download className="pl-12">
+          <Link
+            to="../../assets/cv/Sayed Asif Zayed.pdf"
+            target="_blank"
+            download
+            className="pl-12"
+          >
             <button className="text-black h-[40px] px-10 font-bold bg-gradient-to-r from-[#E2A300] to-[#E29500] rounded-[6px] shadow-[0_2px_13px_rgba(226,158,0,0.48)]">
               Download CV
             </button>
