36be39b9aa2a36fb4901422969ef42c8bce8ea87
Zayed-Fahim <zayedfahim33.cse@gmail.com>
2024-10-27 18:51:57 +0600

    add generateStatticParams to show blog post

--- src/app/blog/[slug]/page.tsx
+++ src/app/blog/[slug]/page.tsx
@@ -1,10 +1,14 @@
 import React from "react";
 
+export async function generateStaticParams() {
+  return {
+    slug: "test-slug",
+  };
+}
+
 const Blog = ({ params }: { params: { slug: string } }) => {
   return (
-    <div>
-      <h1>{params.slug}</h1>
-    </div>
+    <article className="mx-auto max-w-3xl px-4 py-12">{params.slug}</article>
   );
 };
 
